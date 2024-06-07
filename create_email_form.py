013029bba64b7093a5be01e17ea4ec4ea515eaf3
perripez <pezzadkins@gmail.com>
2024-06-07 21:11:34 +1000

    Programs with input

---
+++ create_email_form.py
@@ -0,0 +1,8 @@
+# Perri Adkins Coder Academy -> perri.adkins@coderacademy.com
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+company_name = input("Enter your company name (no blank spaces): ")
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
+
+# email = f"{first_name}.{last_name}@{company_name}.com".lower()
+# print("Your email is: {email}")
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello World")
-print(5-3)
+# AREA OF A SQUARE
+
+# Get the length of the user
+length = float(input("Enter the length of the square: "))
+# print(length)
+
+# Caculate and store the area in a variable (area = length * length)
+area = length * length
+
+# Print the calculated area to the terminal
+print(f"The calculated area is: {area}")
